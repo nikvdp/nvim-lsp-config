14748d3424ab9552c4a96907cb0e3bb18e142a5a
Nik V <nik@nikvdp.com>
2021-05-21 02:10:58 -0700

    move lua configs out to their own files, better treesitter comments

---
+++ lua/treesitter.lua
@@ -0,0 +1,30 @@
+require'nvim-treesitter.configs'.setup {
+    -- ensure_installed can be "all" or a list of languages { "python", "javascript" }
+    ensure_installed = {"python", "bash", "javascript", "clojure", "go"},
+
+    highlight = { -- enable highlighting for all file types
+      enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+    },
+    incremental_selection = {
+      enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+      disable = { "cpp", "lua" },
+      keymaps = {                       -- mappings for incremental selection (visual mappings)
+        init_selection = "gnn",         -- maps in normal mode to init the node/scope selection
+        node_incremental = "grn",       -- increment to the upper named parent
+        scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
+        node_decremental = "grm",       -- decrement to the previous node
+      }
+    },
+    textobjects = {
+      -- These are provided by 
+      select = {
+        enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+        keymaps = {
+          -- You can use the capture groups defined here:
+	  -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/master/queries/c/textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+        },
+      },
+    },
+}
